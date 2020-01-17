10b5e262729ab0e04f91481324e961e3d1805787
Dennis <dennis.olivetti@aalto.fi>
2020-01-17 21:57:01 +0100

    more compiler optimizations

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 cd wasm
 wasm-pack build --target no-modules
 cd ..
-cargo build --release
+RUSTFLAGS="-C target-cpu=native" cargo build --release
 cat wasm/pkg/wasm.js | sed 's/Object.assign(init/Object.assign({init}/' > static/wasm.js
 cp wasm/pkg/wasm_bg.wasm static/
