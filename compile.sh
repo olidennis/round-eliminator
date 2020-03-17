8c3cddeb2367c35ce3f55bc07d7588263c6aed7e
Dennis <dennis.olivetti@aalto.fi>
2020-03-17 14:22:27 +0100

    optimize wasm

--- compile.sh
+++ compile.sh
@@ -4,3 +4,11 @@ cd ..
 RUSTFLAGS="-C target-cpu=native" cargo build --release
 cat wasm/pkg/wasm.js | sed 's/Object.assign(init/Object.assign({init}/' > static/wasm.js
 cp wasm/pkg/wasm_bg.wasm static/
+cd static
+wasm-opt -O3 -o test.wasm wasm_bg.wasm
+mv test.wasm wasm_bg.wasm
+wasm-opt -O3 -o test.wasm wasm_bg.wasm
+mv test.wasm wasm_bg.wasm
+wasm-opt -O3 -o test.wasm wasm_bg.wasm
+mv test.wasm wasm_bg.wasm
+cd ..
