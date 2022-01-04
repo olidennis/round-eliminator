d0d78b9436bb33382d6a3d674e6a91c85fc286ba
Dennis <dennis.olivetti@aalto.fi>
2022-01-04 20:44:39 +0100

    optimized wasm

--- round-eliminator-wasm/build.sh
+++ round-eliminator-wasm/build.sh
@@ -3,3 +3,9 @@
 set -ex
 
 wasm-pack build --out-dir ../www/pkg --target no-modules
+
+cd ../www/pkg
+for i in `seq 1 5`; do
+    wasm-opt -O3 -o test.wasm round_eliminator_wasm_bg.wasm
+    mv test.wasm round_eliminator_wasm_bg.wasm
+done
