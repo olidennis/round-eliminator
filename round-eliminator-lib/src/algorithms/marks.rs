8a43609d0708bc162e205e727b47ed0fdddbfcd2
Dennis <dennis.olivetti@aalto.fi>
2024-06-14 19:11:54 +0200

    faster marks

--- round-eliminator-lib/src/algorithms/marks.rs
+++ round-eliminator-lib/src/algorithms/marks.rs
@@ -1,16 +1,86 @@
-use std::time::Instant;
+use std::{time::Instant};
 
+use bit_vec::BitVec;
 use itertools::{Itertools};
-use rustsat::{instances::SatInstance, solvers::SolverResult, types::constraints::CardConstraint};
+use rand::Rng;
+use rustsat::{instances::SatInstance, solvers::SolverResult, types::{constraints::CardConstraint, Lit}};
 
-use crate::{constraint::Constraint, group::{Group, GroupType}, line::{Degree, Line}, part::Part, problem::Problem};
+use crate::{constraint::Constraint, group::{Group, GroupType, Label}, line::{Degree, Line}, part::Part, problem::Problem};
 use rustsat::solvers::Solve;
 
 use super::event::EventHandler;
 
+use rand::seq::SliceRandom;
 
+type TableIndex = u32;
 
+fn mark_exists<T>(choice : &Vec<TableIndex>, successors : &Vec<Vec<usize>>, handled : &mut BitVec, num_handled : &mut usize, choice_to_index : &T) where T : Fn(&Vec<TableIndex>) -> usize{
+    if handled.get(choice_to_index(choice)).unwrap() {
+        return;
+    }
 
+    handled.set(choice_to_index(choice),true);
+    *num_handled += 1;
+
+    let mut choice = choice.clone();
+
+    for i in 0..choice.len() {
+        let orig_i = choice[i];
+        for &successor in &successors[orig_i as usize] {
+            choice[i] = successor as TableIndex;
+            mark_exists(&choice, successors, handled, num_handled, choice_to_index);
+        }
+        choice[i] = orig_i;
+    }
+}
+
+fn mark_no_exists<T>(choice : &Vec<TableIndex>, successors : &Vec<Vec<usize>>, handled : &mut BitVec, num_handled : &mut usize, table : &Vec<Vec<Lit>>, instance : &mut SatInstance, choice_to_index : &T) where T : Fn(&Vec<TableIndex>) -> usize{
+    if handled.get(choice_to_index(choice)).unwrap() {
+        return;
+    }
+
+    let lits = choice.iter().enumerate().map(|(i,&j)|table[i][j as usize]);
+    instance.add_card_constr(CardConstraint::new_lb(lits, 1));
+
+    handled.set(choice_to_index(choice),true);
+    *num_handled += 1;
+
+    let mut choice = choice.clone();
+
+    for i in 0..choice.len() {
+        let orig_i = choice[i];
+        for &successor in &successors[orig_i as usize] {
+            choice[i] = successor as TableIndex;
+            mark_no_exists(&choice, successors, handled, num_handled, table, instance, choice_to_index);
+        }
+        choice[i] = orig_i;
+    }
+}
+
+fn handle_choice_nodes<T>(active : &Constraint, choice : &Vec<TableIndex>, predecessors : &Vec<Vec<usize>>, successors : &Vec<Vec<usize>>, handled : &mut BitVec, num_handled : &mut usize, table : &Vec<Vec<Lit>>, instance : &mut SatInstance, subsets : &Vec<Vec<Label>>, no_marking : bool, choice_to_index : &T) where T : Fn(&Vec<TableIndex>) -> usize{
+    if handled.get(choice_to_index(choice)).unwrap() {
+        return;
+    }
+
+    let line = Line{parts:choice.iter().map(|j|Part{ 
+        gtype: GroupType::Many(1),
+        group: Group(subsets[*j as usize].clone())
+    }).collect()};
+
+    if no_marking {
+        if !active.exists_choice_in_line(&line) {
+            let lits = choice.iter().enumerate().map(|(i,&j)|table[i][j as usize]);
+            instance.add_card_constr(CardConstraint::new_lb(lits, 1));
+            //handled.insert(choice.clone());
+        }
+    } else {
+        if !active.exists_choice_in_line(&line) {
+            mark_no_exists(&choice,&predecessors,handled, num_handled, &table, instance, choice_to_index);
+        } else {
+            mark_exists(&choice,&successors, handled, num_handled, choice_to_index);
+        }
+    }
+}
 
 impl Problem {
 
@@ -25,6 +95,9 @@ impl Problem {
         }
         
         let labels = self.labels();
+        if labels.len() > std::mem::size_of::<TableIndex>() * 8 {
+            panic!("too many labels");
+        }
         let labels_as_group = Group(labels.clone());
         let degree = self.active.finite_degree();
         let passive_degree = self.passive.finite_degree();
@@ -32,14 +105,33 @@ impl Problem {
         //println!("generating subsets");
 
         let mut subsets = vec![];
+        let mut subsets_as_groups = vec![];
         let mut complements = vec![];
         for subset in labels.iter().cloned().powerset() {
             let subset_as_group = Group(subset.clone());
             let complement = labels_as_group.difference(&subset_as_group);
             complements.push(complement.0);
             subsets.push(subset);
+            subsets_as_groups.push(subset_as_group);
         }
 
+
+        let mut predecessors = vec![];
+        let mut successors = vec![];
+        for subset in 0..subsets.len() {
+            let mut v_pred = vec![];
+            let mut v_succ = vec![];
+            for other in 0..subsets.len() {
+                if subsets_as_groups[subset].is_superset(&subsets_as_groups[other]) && subsets_as_groups[subset].len() == subsets_as_groups[other].len()+1 {
+                    v_pred.push(other);
+                }
+                if subsets_as_groups[other].is_superset(&subsets_as_groups[subset]) && subsets_as_groups[other].len() == subsets_as_groups[subset].len()+1 {
+                    v_succ.push(other);
+                }
+            }
+            predecessors.push(v_pred);
+            successors.push(v_succ);
+        }
 
         //println!("generating variables");
 
@@ -54,26 +146,55 @@ impl Problem {
             table.push(row);
         }
 
-
-        let node_choices = (0..degree).map(|_|0..subsets.len()).multi_cartesian_product();
+        let node_choices = (0..degree).map(|_|0 as TableIndex..subsets.len() as TableIndex).multi_cartesian_product();
         let len = node_choices.clone().count();
         let mut last_notify = chrono::Utc::now().time();
+        let mut rng = rand::thread_rng();
+        let mut sample = node_choices.clone().filter(|_|rng.gen_bool(0.02)).collect_vec();
+        sample.shuffle(&mut rng);
 
-        for (i,choice) in node_choices.enumerate() {
+        let mut handled = BitVec::from_elem(len, false);
+        let mut num_handled = 0;
+
+        let choice_to_index = |v : &Vec<TableIndex>|{
+            let mut p = 0;
+            for x in v {
+                p *= subsets.len();
+                p += *x as usize;
+            }
+            p
+        };
+
+        for (i,choice) in sample.iter().enumerate() {
             let now = chrono::Utc::now().time();
-            if (now - last_notify).num_milliseconds() > 100 {
-                eh.notify("setting up node constraints",i,len);
+            if (now - last_notify).num_milliseconds() > 1000 {
+                eh.notify("setting up node constraints",num_handled,len);
                 last_notify = chrono::Utc::now().time();
+
+                //println!("{} {}",i,num_handled);
             }
             
-            let line = Line{parts:choice.iter().map(|j|Part{ 
-                gtype: GroupType::Many(1),
-                group: Group(subsets[*j].clone())
-            }).collect()};
-            if !self.active.exists_choice_in_line(&line) {
-                let lits = choice.iter().enumerate().map(|(i,&j)|table[i][j]);
-                instance.add_card_constr(CardConstraint::new_lb(lits, 1));
+            handle_choice_nodes(&self.active, &choice, &predecessors, &successors, &mut handled, &mut num_handled, &table, &mut instance, &subsets, false, &choice_to_index);
+            if num_handled > (0.999 * len as f32) as usize {
+                break;
             }
+        }
+        sample.clear();
+        
+        //handled.shrink_to_fit();
+        //println!("done prefiltering");
+        let node_choices = (0..degree).map(|_|0 as TableIndex..subsets.len() as TableIndex).multi_cartesian_product();
+        for (i,choice) in node_choices.enumerate() {
+
+            let now = chrono::Utc::now().time();
+            if (now - last_notify).num_milliseconds() > 1000 {
+                eh.notify("setting up node constraints",i,len);
+                last_notify = chrono::Utc::now().time();
+
+                //println!("{} {}",i,num_handled);
+            }
+            
+            handle_choice_nodes(&self.active, &choice, &predecessors, &successors, &mut handled, &mut num_handled, &table, &mut instance, &subsets, true, &choice_to_index);
         }
 
         //println!("setting up edge constraints");
@@ -103,7 +224,6 @@ impl Problem {
         }
 
         eh.notify("sanitizing",0,0);
-
 
         let instance = instance.sanitize();
 
