68bf316038bf98494e9696a3a7327c1c38d6e663
Dennis <dennis.olivetti@aalto.fi>
2024-08-04 16:37:42 +0200

    re benchmark

---
+++ round-eliminator-benchmark/src/main.rs
@@ -0,0 +1,88 @@
+use round_eliminator_lib::algorithms::event::EventHandler;
+use round_eliminator_lib::problem::Problem;
+use std::time::Instant;
+
+fn test_problem(problem : &str, steps : usize, hash : &str){
+    let mut eh = &mut EventHandler::null();
+    let mut p = Problem::from_string(problem).unwrap();
+    for _ in 0..steps-1 {
+        p = p.speedup(eh);
+        p.passive.maximize(&mut eh);
+        p.compute_partial_diagram(&mut eh);
+        p.sort_active_by_strength();
+        p.compute_passive_gen();
+        p.rename_by_generators().unwrap();
+        p.active.lines.sort();
+        p.passive.lines.sort();
+    }
+    assert!(sha256::digest(std::hint::black_box(p.to_string())) == hash);
+}
+
+fn test_all() {
+    test_problem("M U^9\nP^10\n\nM UP^9\nU^10",11,"d6f90abf897c0ba1bcc4bcb96debf68e56d716d4096c444cd9da0e93f5213219");
+    test_problem("M^10\nP U^9\n\nM UP\nU^2",6,"eb762856d26b16c3b0030971133fb46cf2f6da97089110a8823ae1b0c221fea2");
+    test_problem("(0a) (00b) (00c) (00d) (00e)
+(0a) (00b) (00c) (00d) (01e)
+(0a) (00b) (00c) (01d) (10e)
+(0a) (00b) (00c) (01d) (11e)
+(0a) (00b) (01c) (10d) (00e)
+(0a) (00b) (01c) (10d) (01e)
+(0a) (00b) (01c) (11d) (10e)
+(0a) (00b) (01c) (11d) (11e)
+(0a) (01b) (10c) (00d) (00e)
+(0a) (01b) (10c) (00d) (01e)
+(0a) (01b) (10c) (01d) (10e)
+(0a) (01b) (10c) (01d) (11e)
+(0a) (01b) (11c) (10d) (00e)
+(0a) (01b) (11c) (10d) (01e)
+(0a) (01b) (11c) (11d) (10e)
+(0a) (01b) (11c) (11d) (11e)
+(1a) (10b) (00c) (00d) (00e)
+(1a) (10b) (00c) (00d) (01e)
+(1a) (10b) (00c) (01d) (10e)
+(1a) (10b) (00c) (01d) (11e)
+(1a) (10b) (01c) (10d) (00e)
+(1a) (10b) (01c) (10d) (01e)
+(1a) (10b) (01c) (11d) (10e)
+(1a) (10b) (01c) (11d) (11e)
+(1a) (11b) (10c) (00d) (00e)
+(1a) (11b) (10c) (00d) (01e)
+(1a) (11b) (10c) (01d) (10e)
+(1a) (11b) (10c) (01d) (11e)
+(1a) (11b) (11c) (10d) (00e)
+(1a) (11b) (11c) (10d) (01e)
+(1a) (11b) (11c) (11d) (10e)
+(1a) (11b) (11c) (11d) (11e)
+
+(0a) (1a)
+(00b) (00b)
+(01b) (01b)
+(00b) (10b)
+(01b) (11b)
+(10b) (11b)
+(00c) (00c)
+(01c) (01c)
+(00c) (10c)
+(01c) (11c)
+(10c) (11c)
+(00d) (00d)
+(01d) (01d)
+(00d) (10d)
+(01d) (11d)
+(10d) (11d)
+(00e) (00e)
+(01e) (01e)
+(00e) (10e)
+(01e) (11e)
+(10e) (11e)",2,"8cc0bbf43868b3255c23df6ee760590f999097f843809dbe347b48f63077126d");
+    
+}
+
+
+fn main() {
+    let start = Instant::now();
+    test_all();
+    let duration = start.elapsed();
+    println!("Score (higher is better): {}", 12422 * 8743 / duration.as_millis());
+    //std::env::set_var("RE_NUM_THREADS", "1");    
+}
