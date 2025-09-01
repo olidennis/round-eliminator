c1a30ff6333235e49708c80cbd08849fc87bb5cb
Dennis <dennis.olivetti@aalto.fi>
2025-09-01 21:57:58 +0200

    wasm still crashes on loop, even with non-recursive version of expression_for_line_at

--- round-eliminator-wasm/build.sh
+++ round-eliminator-wasm/build.sh
@@ -9,3 +9,12 @@ for i in `seq 1 5`; do
     wasm-opt -O3 -o test.wasm round_eliminator_wasm_bg.wasm
     mv test.wasm round_eliminator_wasm_bg.wasm
 done
+
+## in this folder:
+# cargo build --target wasm32-unknown-unknown     
+# wasm-bindgen --target no-modules --out-dir ../www/pkg --keep-debug target/wasm32-unknown-unknown/debug/round_eliminator_wasm.wasm
+# cargo wasm2map ../www/pkg/round_eliminator_wasm_bg.wasm
+## in ../www
+# npx serve
+# npx source-map-cli resolve pkg/round_eliminator_wasm_bg.wasm.map 1 wasm-line-where-it-crashed
+# OR: just let it crash on chrome, it writes the stacktrace
